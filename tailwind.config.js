b1eab248e05c5029ee172648b2c435413a08d24e
Ariel Ramos de Braga <bragaariel@gmail.com>
2026-05-28 16:57:11 -0300

    create project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EC4899',
+        secondary: '#A855F7',
+        accent: '#F59E0B',
+        success: '#10B981',
+        error: '#EF4444',
+        muted: '#6B7280',
+      },
+    },
+  },
+  plugins: [],
+};
